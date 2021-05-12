cf05c2d177ea14cd9455e43afb683f1b52b1104c
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-05-12 13:24:52 +0500

    prevented itemCount null  value

--- lib/pages/home/brew_list.dart
+++ lib/pages/home/brew_list.dart
@@ -12,7 +12,7 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     //fatching the all document's data from user's firebase
-    final brews = Provider.of<List<Brew>>(context);
+    final brews = Provider.of<List<Brew>>(context) ?? [];
     return ListView.builder(
       itemCount: brews.length,
       itemBuilder: (context, index) {
