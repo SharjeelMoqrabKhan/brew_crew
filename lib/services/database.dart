3576463f934e24a682d074a1b502b97f8cbcffed
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-05-17 11:15:00 +0500

    User Data Model

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/model/brew_model.dart';
+import 'package:brew_crew/model/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Database {
@@ -27,9 +28,24 @@ class Database {
     ).toList();
   }
 
+  //userData sanpshoot
+  UserData _userDataFromSnapshoot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      strength: snapshot.data['strength'],
+      sugars: snapshot.data['sugars'],
+    );
+  }
+
   // get brew stream if any data changes/updates
   /// Sanpshoot Method Notifies of query results at this location
   Stream<List<Brew>> get brews {
     return brewCollection.snapshots().map(_brewListFromSanpshot);
+  }
+
+  //get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots().map(_userDataFromSnapshoot);
   }
 }
